28945d5fda7e46f6aabc57c784010ffdb7932fa7
Harshit Singhal <harshitsinghal0410@gmail.com>
2022-06-15 20:05:52 +0530

    Use dotenv

--- server.go
+++ server.go
@@ -3,15 +3,22 @@ package main
 import (
 	"JobScheduler/Server/database"
 	"JobScheduler/Server/model"
-	"fmt"
+	"log"
+	"os"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	//	r := mux.NewRouter()
 
-	fmt.Printf("Starting server at port 8080\n")
+	// fmt.Printf("Starting server at port 8080\n")
 
 	// srv := &http.Server{
 	// 	Addr:    ":8080",
@@ -22,12 +29,11 @@ func main() {
 }
 
 func initDB() {
-
 	config := database.Config{
-		Host:     "localhost",
-		User:     "root",
-		Password: "admin",
-		DB:       "job_scheduler",
+		Host:     os.Getenv("HOST"),
+		User:     os.Getenv("USER"),
+		Password: os.Getenv("PASSWORD"),
+		DB:       os.Getenv("DB"),
 	}
 
 	err := database.Connect(config)
@@ -36,5 +42,4 @@ func initDB() {
 	}
 
 	database.Migrate(&model.Job{})
-
 }
